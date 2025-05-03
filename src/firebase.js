54aa9ed93a490d74291a0266a8d9afa4b8deeeb4
Bavi2002 <sivabavithran16@gmail.com>
2025-05-03 23:56:32 +0530

    iuhihuhu

--- src/firebase.js
+++ src/firebase.js
@@ -17,24 +17,29 @@
 // export const auth = getAuth(app);
 // export const db = getFirestore(app);
 
-// firebase.ts or firebaseConfig.ts
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAO9wIZC6O3VXcVPqolwk227ySrcmJWtCU",
+  authDomain: "country-explorer-e8109.firebaseapp.com",
+  databaseURL: "https://country-explorer-e8109-default-rtdb.firebaseio.com",
+  projectId: "country-explorer-e8109",
+  storageBucket: "country-explorer-e8109.firebasestorage.app",
+  messagingSenderId: "816081085815",
+  appId: "1:816081085815:web:3ca3171b53f0bcdedeed72",
+  measurementId: "G-TS8FEJ0JTK",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
